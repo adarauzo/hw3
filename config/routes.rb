11406662a767a85c8d4eb5beb3f849aa1400aa32
Adrian Arauzo Vaillo <adrian.arauzovaillo@kellogg.northwestern.edu>
2024-02-23 12:01:46 +0000

    Added controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'places/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+ resource :places 
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
