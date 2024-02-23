ee76a02ec555659c27b14c3cfd9f42f5f95372fb
Adrian Arauzo Vaillo <adrian.arauzovaillo@kellogg.northwestern.edu>
2024-02-23 20:30:49 +0000

    Last change for using resource: instead of resources:

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = places.find_by({ "id" => params["id"] })
+    @place = Place.find(params[:id])
   end
-
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'places/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
- resource :places 
+  resource :places
+
+  get '/places/:id', to: 'places#show', as: 'place'
+
+
+ 
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
